6f7f53d4333f38e82422fefbe5c8ead8723be57d
mahmad <ahmadqwerty123@gmail.com>
2022-06-12 20:25:07 +0500

    Add a button to change grid size

--- javascript.js
+++ javascript.js
@@ -1,12 +1,28 @@
+const siteBody = document.querySelector(`body`);
+
+const userInput = document.createElement(`button`);
+userInput.textContent = `Squares/Side`;
+siteBody.append(userInput);
+
+userInput.addEventListener(`click`, changeGrid);
+function changeGrid() {
+  const clearPage = document.querySelector(`.container`);
+  clearPage.remove();
+  const gridSide = prompt();
+  mainFunction(gridSide);
+}
+
+const gridSide = 16;
+mainFunction(gridSide);
+function mainFunction (gridSide) {
 const gridLength = 750;
-const gridSide = 64;
 const numTiles = gridSide**2;
 
-const siteBody = document.querySelector(`body`);
 const container = document.createElement(`div`);
 container.classList.add (`container`);
 container.style.display = `grid`;
 container.style.width = `${gridLength}px`;
+
 siteBody.append(container);
 
 let gridLayout = ``;
@@ -25,4 +41,5 @@ for (let i = 0 ; i < numTiles ; i++) {
     gridTile.style.backgroundColor = `black`;
   })
   container.append(gridTile);
+}
 }
